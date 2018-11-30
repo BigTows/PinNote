e0d935bd59f01483c719682bd020ec72e97a8b79
Alexander Chapchuk <gasfull98@gmail.com>
2018-11-30 15:18:03 +0300

    Added exception.

--- src/main/java/org/bigtows/components/exception/PinNoteComponentException.java
+++ src/main/java/org/bigtows/components/exception/PinNoteComponentException.java
@@ -1,4 +1,25 @@
 package org.bigtows.components.exception;
 
-public class PinNoteComponentException {
+/**
+ * The type Pin note component exception.
+ */
+public class PinNoteComponentException extends RuntimeException {
+    /**
+     * Instantiates a new Pin note component exception.
+     *
+     * @param message the message
+     */
+    public PinNoteComponentException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new Pin note component exception.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
+    public PinNoteComponentException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
