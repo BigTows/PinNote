dc086a510dba53c1791f3659e4ad090df82ca28e
Alexander Chapchuk <gasfull98@gmail.com>
2020-05-06 23:28:19 +0300

    Fix exception while creating new target.

--- src/main/java/org/bigtows/window/ui/pinnote/PinNoteComponent.java
+++ src/main/java/org/bigtows/window/ui/pinnote/PinNoteComponent.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTabbedPane;
 import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.components.BorderLayoutPanel;
 import org.bigtows.service.note.notebook.Notebook;
 import org.bigtows.window.ui.border.BottomBorder;
 import org.bigtows.window.ui.notetree.NoteTree;
@@ -26,16 +27,19 @@ public class PinNoteComponent {
 
     private void createUIComponents() {
         notebookTabbedPane = new JBTabbedPane();
-        notebookTabbedPane.removeAll();
         toolBarPanel = new JPanel();
         toolBarPanel.setLayout(new BorderLayout());
+        toolBarPanel.add(this.createActionToolBar());
+    }
+
+    private BorderLayoutPanel createActionToolBar() {
         final DefaultActionGroup group = new DefaultActionGroup();
         group.add(new AddNote(notebookTabbedPane));
         //group.add(new ForceRefreshNoteAction(notebookTabbedPane));
         final ActionToolbar actionToolBar = ActionManager.getInstance().createActionToolbar("PinNoteToolbar", group, true);
         var panel = JBUI.Panels.simplePanel(actionToolBar.getComponent());
         panel.setBorder(new BottomBorder(JBUI.CurrentTheme.ToolWindow.borderColor()));
-        toolBarPanel.add(panel);
+        return panel;
     }
 
     public void addNotebook(Notebook<?> notebook, Icon icon, NoteTree noteTree) {
@@ -46,6 +50,12 @@ public class PinNoteComponent {
         );
     }
 
+    /**
+     * Creating wrapper via Scroll pane for NoteTree
+     *
+     * @param noteTree instance of NoteTree
+     * @return Scroll pane with NoteTree
+     */
     private JScrollPane createScrollPaneWithNoteTree(NoteTree noteTree) {
         var scroll = new JBScrollPane(noteTree, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scroll.setBorder(BorderFactory.createEmptyBorder());
