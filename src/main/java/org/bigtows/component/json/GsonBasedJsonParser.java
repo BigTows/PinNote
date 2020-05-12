1647f819fc5a3fbed9152624b160152fab906e5e
Alexander Chapchuk <gasfull98@gmail.com>
2020-05-12 18:46:52 +0300

    Refactor code.

--- src/main/java/org/bigtows/component/json/GsonBasedJsonParser.java
+++ src/main/java/org/bigtows/component/json/GsonBasedJsonParser.java
@@ -2,18 +2,29 @@ package org.bigtows.component.json;
 
 import com.google.gson.Gson;
 
-public class GoonBasedJsonParser implements JsonParser {
+/**
+ * Implements of json parser.
+ * Bassed on gson
+ */
+public class GsonBasedJsonParser implements JsonParser {
 
+    /**
+     * Instance of gson.
+     */
     private Gson instance;
 
-    public GoonBasedJsonParser() {
+    /**
+     * Constructor
+     */
+    public GsonBasedJsonParser() {
         this.instance = new Gson();
     }
 
+    @Override
     public <T> T parse(String content, Class<T> clazz) {
         return this.instance.fromJson(content, clazz);
     }
-
+    @Override
     public String toJson(Object object) {
         return this.instance.toJson(object);
     }
--- src/main/java/org/bigtows/component/json/JsonParser.java
+++ src/main/java/org/bigtows/component/json/JsonParser.java
@@ -2,12 +2,27 @@ package org.bigtows.component.json;
 
 import com.intellij.openapi.components.Service;
 
+/**
+ * Interface of json parser
+ */
 @Service
 public interface JsonParser {
 
-
+    /**
+     * Parse content and get instance of T
+     *
+     * @param content json content
+     * @param clazz   clazz
+     * @param <T>     generic of class
+     * @return instance of T
+     */
     <T> T parse(String content, Class<T> clazz);
 
-
+    /**
+     * Create json-content by object
+     *
+     * @param object target object
+     * @return json-content
+     */
     String toJson(Object object);
 }
