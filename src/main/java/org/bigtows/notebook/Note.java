975c3d99653d8192fff94c2f0cc1ac6d1005c359
Alexander Chapchuk <gasfull98@gmail.com>
2020-05-12 18:52:25 +0300

    Refactor code.

--- src/main/java/org/bigtows/notebook/Note.java
+++ src/main/java/org/bigtows/notebook/Note.java
@@ -3,16 +3,31 @@ package org.bigtows.notebook;
 import java.util.List;
 
 /**
- * Note of
- * @param <T>
+ * Note this is an entity that stores issues
+ *
+ * @param <T> any implementation of Task
  */
 public interface Note<T extends Task> {
 
-
+    /**
+     * Get id note
+     *
+     * @return id of note
+     */
     String getId();
 
+    /**
+     * Get name
+     *
+     * @return name of note
+     */
     String getName();
 
+    /**
+     * Set name
+     *
+     * @param name of note
+     */
     void setName(String name);
 
     List<T> getTasks();
--- src/main/java/org/bigtows/notebook/Notebook.java
+++ src/main/java/org/bigtows/notebook/Notebook.java
@@ -2,14 +2,39 @@ package org.bigtows.notebook;
 
 import java.util.List;
 
+/**
+ * Notebook this is an entity that stores notes
+ *
+ * @param <T> any implementation of Note
+ */
 public interface Notebook<T extends Note<? extends Task>> {
 
-    public String getName();
+    /**
+     * Get name of notebook
+     *
+     * @return name of notebook
+     */
+    String getName();
 
-
+    /**
+     * Update notes
+     *
+     * @param notes collection of notes
+     * @return updated collection
+     */
     List<T> updateNotes(List<T> notes);
 
+    /**
+     * Get all notes
+     *
+     * @return list of notes
+     */
     List<T> getAllNotes();
 
+    /**
+     * Delete note from collection notes
+     *
+     * @param note instance from collection
+     */
     void deleteNote(T note);
 }
